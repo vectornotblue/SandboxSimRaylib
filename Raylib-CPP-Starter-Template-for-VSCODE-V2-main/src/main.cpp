263a18509d9924d6c794b5b99583b804ca196708
Vector Blue <modrivektor@gmail.com>
2026-02-15 20:35:43 +0100

    Enums, Dispersion

--- Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
@@ -3,7 +3,13 @@
 
 int main() 
 {
-    
+    enum Elm{
+        NONE = 0,
+        SAND = 1,
+        STONE = 2,
+        WATER = 3
+
+    };
     const int screenWidth = 1920;
     const int screenHeight = 1080;
     const int arrayRows = 192;
@@ -23,8 +29,8 @@ int main()
         if(IsKeyPressed(KEY_R)){
             for(int i = 0; i < arrayRows; i++){
                 for(int j = 0; j < arrayColumns; j++){
-                    array[i][j] = 0; 
-                    tempArray[i][j] = 0;
+                    array[i][j] = NONE; 
+                    tempArray[i][j] = NONE;
                 }
             }
         }
@@ -33,53 +39,58 @@ int main()
             lastTime = currentTime;
             for(int i = 0; i<arrayRows; i++){
                 for(int j = 0; j < arrayColumns; j++){
-                    if(array[i][j] ==2)
+                    if(array[i][j] ==STONE)
                     {
-                        tempArray[i][j] =2;
-                    }else if((array[i][j] == 1)&&(j+1 <arrayColumns))
+                        tempArray[i][j] =STONE;
+                    }else if((array[i][j] == SAND)&&(j+1 <arrayColumns))
                     {
                         int checkDir = GetRandomValue(0,1) == 0 ? -1:1;
-                        if(tempArray[i][j+1] != 0){
-                            if(tempArray[i+checkDir][j+1] ==0 && tempArray[i+checkDir][j]==0){
-                                tempArray[i][j] = 0;
-                                tempArray[i+checkDir][j+1] = 1;
-                            }else if(tempArray[i-checkDir][j+1] ==0&& tempArray[i-checkDir][j]==0){
-                                tempArray[i][j] = 0;
-                                tempArray[i-checkDir][j+1] = 1;
+                        if(tempArray[i][j+1] != NONE){
+                            
+                            if(tempArray[i+checkDir][j+1] !=STONE&& tempArray[i+checkDir][j+1] !=SAND && tempArray[i+checkDir][j]!=STONE && tempArray[i+checkDir][j] !=SAND) {
+                                tempArray[i][j] = tempArray[i+checkDir][j+1];
+                                tempArray[i+checkDir][j+1] = SAND;
+                            }else if(tempArray[i-checkDir][j+1] !=STONE && tempArray[i-checkDir][j+1] !=SAND && tempArray[i-checkDir][j]!=STONE && tempArray[i-checkDir][j] !=SAND){
+                                tempArray[i][j] = tempArray[i-checkDir][j+1];
+                                tempArray[i-checkDir][j+1] = SAND;
                             }else{
-                                tempArray[i][j] = 1;
+                                tempArray[i][j] = SAND;
                             }    
                         }else{
-                            tempArray[i][j] = 0;
-                            tempArray[i][j+1] = 1;
+                            tempArray[i][j] = NONE;
+                            tempArray[i][j+1] = SAND;
                         }
-                    } else if((array[i][j] ==3) &&(j+1 <arrayColumns)){
+                    } else if((array[i][j] == WATER) &&(j+1 <arrayColumns)){
 
                         int checkDir = GetRandomValue(0,1) == 0 ? -1:1;
                         if(tempArray[i][j+1] != 0){
-                            if(tempArray[i+checkDir][j+1] ==0 && tempArray[i+checkDir][j]==0){
-                                tempArray[i][j] = 0;
-                                tempArray[i+checkDir][j+1] = 3;
-                            }else if(tempArray[i-checkDir][j+1] ==0&& tempArray[i-checkDir][j]==0){
-                                tempArray[i][j] = 0;
-                                tempArray[i-checkDir][j+1] = 3;
+                            if(tempArray[i+checkDir][j+1] ==NONE && tempArray[i+checkDir][j]==NONE){
+                                tempArray[i][j] = NONE;
+                                tempArray[i+checkDir][j+1] = WATER;
+                            }else if(tempArray[i-checkDir][j+1] ==NONE && tempArray[i-checkDir][j]==NONE){
+                                tempArray[i][j] = NONE;
+                                tempArray[i-checkDir][j+1] = WATER;
                             }else{
                                 int checkDir2 = GetRandomValue(0,1) == 0 ? -1:1;
-                                bool foundNewCell = false;/* WATER NOT DISPERSING YET
-                                for(int k = 0; !foundNewCell && k < waterDispersity; k++){
-                                    if(tempArray[i+k*checkDir2][j] ==0){
-                                        tempArray[i+k*checkDir2][j] = 3;
+                                bool foundNewCell = false;
+                                bool blockedByStone = false;
+                                for(int k = 0; !foundNewCell && !blockedByStone && k < waterDispersity; k++){
+                                    if(tempArray[i+k*checkDir2][j] ==NONE){
+                                        tempArray[i+k*checkDir2][j] = WATER;
                                         foundNewCell = true;
-                                    }else if (tempArray[i-k*checkDir2][j]){
-                                        tempArray[i-k*checkDir2][j] = 3;
+                                    }else if (tempArray[i-k*checkDir2][j] ==NONE){
+                                        tempArray[i-k*checkDir2][j] = WATER;
                                         foundNewCell = true;
+                                    }else if((tempArray[i+k*checkDir2][j] == STONE) || (tempArray[i-k*checkDir2][j] == STONE)){
+                                        blockedByStone = true;
+                                        
                                     }
-                                } */
-                                tempArray[i][j] = foundNewCell? 0:3;
+                                } 
+                                tempArray[i][j] = foundNewCell? NONE:WATER;
                             }    
                         }else{
-                            tempArray[i][j] = 0;
-                            tempArray[i][j+1] = 3;
+                            tempArray[i][j] = NONE;
+                            tempArray[i][j+1] = WATER;
                         }
                     }
                 }   
